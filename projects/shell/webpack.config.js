cdc26f9692a2cd21d70d2a51114fdd80fb1a6432
Manfred Steyer <manfred.steyer@gmx.net>
2020-11-04 21:44:23 +0100

    fin tutorial

---
+++ projects/shell/webpack.config.js
@@ -0,0 +1,25 @@
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+    module.exports = {
+      output: {
+        publicPath: "http://localhost:5000/",
+        uniqueName: "shell"
+      },
+      optimization: {
+        // Only needed to bypass a temporary bug
+        runtimeChunk: false
+      },
+      plugins: [
+        new ModuleFederationPlugin({
+          
+         
+           
+            // For hosts (please adjust)
+            remotes: {
+                'mfe1': "mfe1@http://localhost:3000/remoteEntry.js" 
+            },
+
+            shared: ["@angular/core", "@angular/common", "@angular/router"]
+        })
+      ],
+    };
