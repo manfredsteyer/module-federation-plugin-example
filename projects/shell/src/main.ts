dee86be58c2ff8ef797bb231f345ae508b8cef65
Manfred Steyer <manfred.steyer@gmx.net>
2020-11-20 22:41:50 +0100

    baseline

--- projects/shell/src/main.ts
+++ projects/shell/src/main.ts
@@ -1,3 +1,12 @@
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
 
+if (environment.production) {
+  enableProdMode();
+}
 
-  import('./bootstrap');
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
+  
