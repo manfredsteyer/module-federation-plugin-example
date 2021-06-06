3e0704c92beba028cb53c8d295b35406b1a7de8b
Manfred Steyer <manfred.steyer@gmx.net>
2021-06-06 11:38:19 +0200

    use mf-tools

--- projects/shell/src/app/app.component.ts
+++ projects/shell/src/app/app.component.ts
@@ -2,12 +2,15 @@ import { Component, NgZone } from '@angular/core';
 import { AuthLibService } from 'auth-lib';
 import { shareNgZone } from '@angular-architects/module-federation-tools';
 
+declare const require: any;
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
 })
 export class AppComponent {
   title = 'shell';
+  ngVersion = require('../../../../package.json').dependencies['@angular/core'];
 
   constructor(private service: AuthLibService, private ngZone: NgZone) {
     shareNgZone(ngZone);
