e8dc23a71f0d9a94b8da074b198c4c307e8e81d8
Manfred Steyer <manfred.steyer@gmx.net>
2021-07-08 15:13:55 +0200

    done

--- projects/mfe1/webpack.config.js
+++ projects/mfe1/webpack.config.js
@@ -2,6 +2,8 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const mf = require("@angular-architects/module-federation/webpack");
 const path = require("path");
 
+const share = mf.share;
+
 const sharedMappings = new mf.SharedMappings();
 sharedMappings.register(
   path.join(__dirname, '../../tsconfig.json'),
@@ -30,15 +32,15 @@ module.exports = {
         exposes: {
             './Module': './projects/mfe1/src/app/flights/flights.module.ts',
         },        
-        shared: {
-          "@angular/core": { singleton: true, strictVersion: true, requiredVersion: '12.0.0' },
-          "@angular/common": { singleton: true, strictVersion: true, requiredVersion: '12.0.0' },
-          "@angular/router": { singleton: true, strictVersion: true, requiredVersion: '12.0.0' },
-          "@angular/common/http": { singleton: true, strictVersion: true, requiredVersion: '12.0.0' }, 
+        shared: share({
+          "@angular/core": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+          "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+          "@angular/router": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+          "@angular/common/http": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
   
           // Uncomment for sharing lib of an Angular CLI or Nx workspace
           ...sharedMappings.getDescriptors()
-        }
+        })
         
     }),
     // Uncomment for sharing lib of an Angular CLI or Nx workspace
