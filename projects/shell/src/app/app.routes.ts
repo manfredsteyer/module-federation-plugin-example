6a0c80221146c849707aff9c1ed48e7806b675ca
Manfred Steyer <manfred.steyer@gmx.net>
2022-06-05 22:49:49 +0200

    Part 4c

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -16,8 +16,8 @@ export const APP_ROUTES: Routes = [
       path: 'flights',
       loadChildren: () =>
           loadRemoteModule({
-              type: 'module',
-              remoteEntry: 'http://localhost:4201/remoteEntry.js',
+              type: 'manifest',
+              remoteName: 'mfe1',
               exposedModule: './Module'
           })
           .then(m => m.FlightsModule)
