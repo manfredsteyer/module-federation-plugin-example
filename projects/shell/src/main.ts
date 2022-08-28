fe4dcf5b5956416b5ab1da9f949c34470593b411
Manfred Steyer <manfred.steyer@gmx.net>
2022-08-28 20:47:33 +0200

    step 4b

--- projects/shell/src/main.ts
+++ projects/shell/src/main.ts
@@ -1,2 +1,11 @@
-import('./bootstrap')
-	.catch(err => console.error(err));
+import { loadRemoteEntry } from '@angular-architects/module-federation';
+
+Promise.all([
+  loadRemoteEntry({
+	type: 'module',
+	remoteEntry: 'http://localhost:4201/remoteEntry.js',
+  }),
+])
+.catch((err) => console.error('Error loading remote entries', err))
+.then(() => import('./bootstrap'))
+.catch((err) => console.error(err));
