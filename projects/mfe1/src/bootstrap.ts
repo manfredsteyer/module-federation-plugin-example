467fd55ea717f9b27fd41c40fa98383d96541db2
Manfred Steyer <manfred.steyer@gmx.net>
2022-08-28 21:30:35 +0200

    step 6

--- projects/mfe1/src/bootstrap.ts
+++ projects/mfe1/src/bootstrap.ts
@@ -1,11 +1,8 @@
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
+import { bootstrap } from '@angular-architects/module-federation-tools';
 
-if (environment.production) {
-  enableProdMode();
-}
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrap(AppModule, {
+    production: environment.production,
+    appType: 'shell',
+});
--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { loadRemoteModule } from '@angular-architects/module-federation';
+import { startsWith, WebComponentWrapper, WebComponentWrapperOptions } from '@angular-architects/module-federation-tools';
 
 export const APP_ROUTES: Routes = [
   {
@@ -21,6 +22,90 @@ export const APP_ROUTES: Routes = [
         })
         .then(m => m.FlightsModule)
   },
+
+  {
+    path: 'react',
+    component: WebComponentWrapper,
+    data: {
+        type: 'script',
+        remoteEntry: 'https://witty-wave-0a695f710.azurestaticapps.net/remoteEntry.js',
+        remoteName: 'react',
+        exposedModule: './web-components',
+        elementName: 'react-element'
+    } as WebComponentWrapperOptions
+},
+
+{
+    path: 'angular1',
+    component: WebComponentWrapper,
+    data: {
+        type: 'script',
+        remoteEntry: 'https://nice-grass-018f7d910.azurestaticapps.net/remoteEntry.js',
+        remoteName: 'angular1',
+        exposedModule: './web-components',
+        elementName: 'angular1-element'
+    } as WebComponentWrapperOptions
+},
+
+{
+    path: 'angular2',
+    component: WebComponentWrapper,
+    data: {
+        type: 'script',
+        remoteEntry: 'https://gray-pond-030798810.azurestaticapps.net//remoteEntry.js',
+        remoteName: 'angular2',
+        exposedModule: './web-components',
+        elementName: 'angular2-element'
+    } as WebComponentWrapperOptions
+},
+
+{
+    matcher: startsWith('angular3'),
+    component: WebComponentWrapper,
+    data: {
+        type: 'script',
+        remoteEntry: 'https://gray-river-0b8c23a10.azurestaticapps.net/remoteEntry.js',
+        remoteName: 'angular3',
+        exposedModule: './web-components',
+        elementName: 'angular3-element'
+    } as WebComponentWrapperOptions
+},
+
+{
+    path: 'vue',
+    component: WebComponentWrapper,
+    data: {
+        type: 'script',
+        remoteEntry: 'https://mango-field-0d0778c10.azurestaticapps.net/remoteEntry.js',
+        remoteName: 'vue',
+        exposedModule: './web-components',
+        elementName: 'vue-element'
+    } as WebComponentWrapperOptions
+},
+
+{
+    path: 'angularjs',
+    component: WebComponentWrapper,
+    data: {
+        type: 'script',
+        remoteEntry: 'https://calm-mud-0a3ee4a10.azurestaticapps.net/remoteEntry.js',
+        remoteName: 'angularjs',
+        exposedModule: './web-components',
+        elementName: 'angularjs-element'
+    } as WebComponentWrapperOptions
+},
+
+{
+    matcher: startsWith('angular3'),
+    component: WebComponentWrapper,
+    data: {
+        type: 'script',
+        remoteEntry: 'https://gray-river-0b8c23a10.azurestaticapps.net/remoteEntry.js',
+        remoteName: 'angular3',
+        exposedModule: './web-components',
+        elementName: 'angular3-element'
+    } as WebComponentWrapperOptions
+},
 
   {
     path: '**',
