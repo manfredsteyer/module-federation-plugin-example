b2b58738b7d821c080c72dcb38c5f5354b1cfd29
Manfred Steyer <manfred.steyer@gmx.net>
2022-08-28 20:41:15 +0200

    step 4a

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -1,29 +1,32 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
 export const APP_ROUTES: Routes = [
-    {
-      path: '',
-      component: HomeComponent,
-      pathMatch: 'full'
-    },
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
 
+  // Your route here:
 
-    // Your route here:
+  {
+    path: 'flights',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        exposedModule: './Module',
+      }).then((m) => m.FlightsModule),
+  },
 
-    {
-      path: 'flights',
-      loadChildren: () => import('mfe1/Module').then(m => m.FlightsModule)
-    },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
 
-    {
-      path: '**',
-      component: NotFoundComponent
-    }
-
-    // DO NOT insert routes after this one.
-    // { path:'**', ...} needs to be the LAST one.
-
+  // DO NOT insert routes after this one.
+  // { path:'**', ...} needs to be the LAST one.
 ];
-
--- projects/shell/webpack.config.js
+++ projects/shell/webpack.config.js
@@ -3,7 +3,7 @@ const { shareAll, withModuleFederationPlugin } = require('@angular-architects/mo
 module.exports = withModuleFederationPlugin({
 
   remotes: {
-    'mfe1': 'http://localhost:4201/remoteEntry.js',
+    // 'mfe1': 'http://localhost:4201/remoteEntry.js',
   },
 
   shared: {
