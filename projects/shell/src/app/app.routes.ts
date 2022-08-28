5e7fd35ca8be79da426a20289868b46634bd89d8
Manfred Steyer <manfred.steyer@gmx.net>
2022-08-28 20:54:02 +0200

    step 4c

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -11,15 +11,15 @@ export const APP_ROUTES: Routes = [
   },
 
   // Your route here:
-
   {
     path: 'flights',
     loadChildren: () =>
-      loadRemoteModule({
-        type: 'module',
-        remoteEntry: 'http://localhost:4201/remoteEntry.js',
-        exposedModule: './Module',
-      }).then((m) => m.FlightsModule),
+        loadRemoteModule({
+            type: 'manifest',
+            remoteName: 'mfe1',
+            exposedModule: './Module'
+        })
+        .then(m => m.FlightsModule)
   },
 
   {
--- projects/shell/src/main.ts
+++ projects/shell/src/main.ts
@@ -1,11 +1,6 @@
-import { loadRemoteEntry } from '@angular-architects/module-federation';
+import { loadManifest } from '@angular-architects/module-federation';
 
-Promise.all([
-  loadRemoteEntry({
-	type: 'module',
-	remoteEntry: 'http://localhost:4201/remoteEntry.js',
-  }),
-])
-.catch((err) => console.error('Error loading remote entries', err))
-.then(() => import('./bootstrap'))
-.catch((err) => console.error(err));
+loadManifest('assets/mf.manifest.json')
+  .catch((err) => console.error('Error loading remote entries', err))
+  .then(() => import('./bootstrap'))
+  .catch((err) => console.error(err));
