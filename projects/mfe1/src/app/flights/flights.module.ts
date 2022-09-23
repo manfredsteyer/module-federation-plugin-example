ac2bb59bcff23ab9ef1e4afd71d59ae389b02c92
VineedKaladharan <vineedkumar.kaladharan@gmail.com>
2022-09-23 08:56:05 +0000

    first commit

--- projects/mfe1/src/app/flights/flights.module.ts
+++ projects/mfe1/src/app/flights/flights.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FlightsSearchComponent } from './flights-search/flights-search.component';
 import { RouterModule } from '@angular/router';
 import { FLIGHTS_ROUTES } from './flights.routes';
-import { AuthLibModule } from 'auth-lib';
-import { SharedLibModule } from 'shared-lib';
+import { AuthLibModule } from './../../../../auth-lib/src/public-api';
+import { SharedLibModule } from './../../../../shared-lib/src/lib/shared-lib.module';
 
 @NgModule({
   imports: [
