e0f0d936079a6d42bc995cf2c8aab0a6f35ea569
Manfred Steyer <manfred.steyer@gmx.net>
2023-05-06 20:45:34 +0200

    update to ng16

--- update.bat
+++ update.bat
@@ -1,5 +1,5 @@
 rd .angular /s /Q
 rd node_modules\@angular-architects\module-federation /s /q   
 rd node_modules\@angular-architects\module-federation-runtime /s /q   
-xcopy D:\Dokumente\projekte\mf-plugin\angular-architects\dist\libs\mf node_modules\@angular-architects\module-federation /s /e /i                
-xcopy D:\Dokumente\projekte\mf-plugin\angular-architects\dist\libs\mf-runtime node_modules\@angular-architects\module-federation-runtime /s /e /i
+xcopy ..\angular-architects\dist\libs\mf node_modules\@angular-architects\module-federation /s /e /i                
+xcopy ..\angular-architects\dist\libs\mf-runtime node_modules\@angular-architects\module-federation-runtime /s /e /i
